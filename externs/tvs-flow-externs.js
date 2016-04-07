3d8bd31b915cc3000f16c0ec4e029c3e6180d508
trival <thomas.gorny@mail.com>
2016-04-07 18:37:29 +0200

    add extern declaration for google closure compiler

--- externs/tvs-flow-externs.js
+++ externs/tvs-flow-externs.js
@@ -1,1 +1,20 @@
 var tvsFlow;
+
+tvsFlow.create = function() {
+
+  return {
+    get: function() {},
+    set: function() {},
+    update: function() {},
+    on: function() {},
+    off: function() {},
+    getState: function() {},
+    addEntity: function() {},
+    removeEntity: function() {},
+    addProcess: function() {},
+    removeProcess: function() {},
+    addArc: function() {},
+    removeArc: function() {},
+    start: function() {}
+  }
+}
