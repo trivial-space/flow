3ec6cc661805ef7b3876c41e450eff867d4029ce
trival <thomas.gorny@mail.com>
2016-06-18 21:22:59 +0200

    get rid of uuid dependency

---
+++ spec/lib/utils/uuid-test.js
@@ -0,0 +1,24 @@
+import {v4} from 'utils/uuid'
+
+
+describe('UUID', function() {
+
+  it('generates random strings', function() {
+    let u1 = v4(),
+        u2 = v4(),
+        u3 = v4(),
+        u4 = v4()
+
+    expect(u1).to.be.a('string')
+    expect(u2).to.be.a('string')
+    expect(u3).to.be.a('string')
+    expect(u4).to.be.a('string')
+
+    expect(u1).to.not.equal(u2)
+    expect(u1).to.not.equal(u3)
+    expect(u1).to.not.equal(u4)
+    expect(u2).to.not.equal(u3)
+    expect(u2).to.not.equal(u4)
+    expect(u3).to.not.equal(u4)
+  })
+})
