7b1560b092b67e6ee01fb0a19de6f4d90d5dbee4
trival <thomas.gorny@mail.com>
2016-07-31 19:57:38 +0200

    fix test build

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path'),
 
 module.exports = {
 
-  entry: path.resolve(__dirname, "./lib/runtime.js"),
+  entry: path.resolve(__dirname, "./lib/index.js"),
 
   output: {
     path: path.resolve(__dirname, "./dist"),
