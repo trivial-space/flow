b18c07c15279e09ca78cc2195681cd70bbf8b5ca
trival <thomas.gorny@mail.com>
2016-11-26 17:39:59 +0100

    remove old files

--- lib/runtime.d.ts
+++
@@ -1,2 +0,0 @@
-import * as types from './runtime-types';
-export declare function create(): types.Runtime;
--- src/utils/code-evaluator.ts
+++
@@ -1,8 +0,0 @@
-import {Procedure} from '../runtime-types'
-
-export function evaluate(code: string, context?: any): Procedure {
-  const prefix = "(function(){ return "
-  const postfix = "})"
-  const factory = eval(prefix + code + postfix) as Function
-  return factory.call(context)
-}
